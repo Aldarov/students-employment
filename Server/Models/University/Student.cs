0ea4ffc3a89c27d89971092b7f18abd3bd485449
Aldarov <alex_aldarov@mail.ru>
2018-04-20 16:26:05 +0800

    bug fix

--- Server/Models/University/Student.cs
+++ Server/Models/University/Student.cs
@@ -10,23 +10,23 @@ namespace Server.Models.University
         public Student()
         {
             ContractStuffs = new HashSet<PgContractStuff>();
-        }        
+        }
 
         [Key]
         public int StudentId { get; set; }
         public string FullName { get; set; }
         public string RegAddress { get; set; }
-        public int FinanceId { get; set; }
+        public int? FinanceId { get; set; }
         public string Finance { get; set; }
-        public int EntrTypeId { get; set; }
+        public int? EntrTypeId { get; set; }
         public string EntrType { get; set; }
         public string Phone { get; set; }
-        public int StateId { get; set; }
+        public int? StateId { get; set; }
         public string State { get; set; }
-        public int SpecialityId { get; set; }
-        public int EducationFormId { get; set; }
-        public int EntranceYear { get; set; }
-        public int EntranceYearByOrder { get; set; }
+        public int? SpecialityId { get; set; }
+        public int? EducationFormId { get; set; }
+        public int? EntranceYear { get; set; }
+        public int? EntranceYearByOrder { get; set; }
         [JsonIgnore]
         public virtual ICollection<PgContractStuff> ContractStuffs { get; set; }
     }

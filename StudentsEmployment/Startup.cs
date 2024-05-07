fd4e0c1046cd0aab082f31c97034cb11c0df1f2a
a.aldarov <alex_aldarov@mail.ru>
2024-05-07 09:36:32 +0800

    fix

--- StudentsEmployment/Startup.cs
+++ StudentsEmployment/Startup.cs
@@ -89,9 +89,8 @@ namespace StudentsEmployment
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseStaticFiles();
             app.UseRouting();
